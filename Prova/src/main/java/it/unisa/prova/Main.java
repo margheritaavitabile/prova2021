603da658f72f1d5a0508b10857b28609843c923d
meggy <meggy@host.docker.internal>
2021-11-11 15:43:22 +0100

    Primo commit

---
+++ Prova/src/main/java/it/unisa/prova/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it.unisa.prova;
+
+/**
+ *
+ * @author meggy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello word!");
+    }
+    
+}
