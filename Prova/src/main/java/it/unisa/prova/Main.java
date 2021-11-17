70abc72ef81b494f7098e6769eb134ae7d36416c
meggy <meggy@host.docker.internal>
2021-11-17 09:25:04 +0100

    prova vera

--- Prova/src/main/java/it/unisa/prova/Main.java
+++ Prova/src/main/java/it/unisa/prova/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello word!");
         System.out.print("Hello universe!");
+        System.out.print("Hello!");
     }
     
 }
